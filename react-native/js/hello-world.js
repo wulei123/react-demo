799515aa81ad8d00ce1e1b1f4c95a0366e7c2e3c
wulei123 <1534563365@qq.com>
2017-01-25 16:30:56 +0800

    state

--- react-native/js/hello-world.js
+++ react-native/js/hello-world.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react'
 import {Text,View,StyleSheet} from 'react-native'
 
 export default class HelloWorld extends Component{
+	constructor(props) {
+	  super(props);
+	  this.state = {showText:true};
+	  setInterval(()=>{this.setState({showText:!this.state.showText})},1000)
+	}
 	render(){
+		let text = this.state.showText ? 'Hello World !!!' : ''
 		return (
 			<View>
-				<Text style={styles.hello}>Hello World !!!</Text>
+				<Text style={styles.hello}>{text}</Text>
 			</View>
 			)
 	}
