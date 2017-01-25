03d1516169595d38e88aed273faf225abf4e0845
wulei123 <1534563365@qq.com>
2017-01-25 17:08:53 +0800

    flex

--- react-native/index.android.js
+++ react-native/index.android.js
@@ -3,13 +3,16 @@ import {View,AppRegistry,} from 'react-native'
 import HelloWorld from './js/hello-world'
 import MyImage from './js/my-image'
 
-
 export default class Main extends Component{
   render(){
     return (
-      <View>
-      <MyImage src={'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'} style={{width: 193,height: 110}} />
-      <HelloWorld />
+      <View style={{flex:1,backgroundColor:'powderblue'}}>
+      <View style={{flex:4}}>
+        <MyImage src={'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'} style={{width: 193,height: 110}} />
+        <HelloWorld />
+      </View>
+          <View style={{flex:1,backgroundColor:'skyblue'}}></View>
+      <View style={{flex:2,backgroundColor:'steelblue'}}></View>
       </View>
       )
   }
